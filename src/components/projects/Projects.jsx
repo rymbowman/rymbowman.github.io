f95895410d1323d4814f9c65a5408cd7b80a3277
Ryan Bowman <rymbowman@gmail.com>
2025-02-04 10:23:30 -0500

    update projects container to be responsive on multiple screen sizes and remove the use of ProjectCard component

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../projects/Projects.css";
-import ProjectCard from "./ProjectCard";
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -8,29 +7,59 @@ const Projects = () => {
   useEffect(() => {
     AOS.init({ duration: 2500 });
   }, []);
+
+  const projects = [
+    {
+      id: "fantasy-football",
+      link: "https://fantasy-football-website.onrender.com",
+      githubLink: "https://github.com/rymbowman/Fantasy-Football-Website",
+      title: "Fantasy Football Website (React)",
+    },
+    {
+      id: "blog-website",
+      link: "",
+      githubLink: "",
+      title: "Blog Website (React, Node.js, PostgreSQL)",
+    },
+    {
+      id: "rock-paper-scissors",
+      link: "",
+      githubLink: "",
+      title: "Rock-Paper-Scissors (JS)",
+    },
+    {
+      id: "todo-app",
+      link: "",
+      githubLink: "",
+      title: "To-do Application (React)",
+    },
+  ];
+
   return (
-    <>
+    <div className="projects-container" data-aos="fade-in">
       <h2 id="projects-title" data-aos="fade-in">
         Projects
       </h2>
-      <div id="projects" data-aos="fade-in">
-        <ProjectCard
-          id={"dynasty"}
-          link={""}
-          title={"Fantasy Football Website (React)"}
-        />
-        <ProjectCard
-          id={"etch-a-sketch"}
-          link={""}
-          title={"Etch-A-Sketch (Javascript)"}
-        />
-        <ProjectCard
-          id={"rock-paper-scissors"}
-          link={""}
-          title={"Rock-Paper-Scissors (Javascript)"}
-        />
+      <div className="projects-tileContainer" data-aos="fade-in">
+        {projects.map((project) => (
+          <div key={project.id} className="project-tile">
+            <h3 className="project-title">{project.title}</h3>
+            <div className="project-links">
+              <a href={project.link} target="_blank" rel="noopener noreferrer">
+                Live Site
+              </a>
+              <a
+                href={project.githubLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub Repo
+              </a>
+            </div>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
