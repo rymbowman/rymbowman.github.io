ddaf2d6b8bf384df97fccac1aaaf6779710b8dab
Ryan Bowman <rymbowman@gmail.com>
2025-02-04 10:35:58 -0500

    update import route to App.css and include future warning preventions for React updates

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,31 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import "./App.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<Homepage />} />
       <Route path="/connect" element={<Connect />} />
-      <Route path="/resume" />
       <Route
         path="*"
         element={<h1>Page not found. Please return to home page.</h1>}
       />
     </Route>
-  )
+  ),
+  {
+    future: {
+      v7_skipActionErrorRevalidation: true,
+      v7_partialHydration: true,
+      v7_normalizeFormMethod: true,
+      v7_fetcherPersist: true,
+      v7_relativeSplatPath: true,
+      v7_startTransition: true,
+    },
+  }
 );
+
 function App() {
   return <RouterProvider router={router} />;
 }
