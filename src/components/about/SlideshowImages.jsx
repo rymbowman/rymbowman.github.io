61994806fd061febd07d587dd739988702864651
Ryan Bowman <rymbowman@gmail.com>
2025-02-05 08:59:23 -0500

    update currentSlide function to cycle back to first image in slides array instead of cycling back to 2nd image in array

--- src/components/about/SlideshowImages.jsx
+++ src/components/about/SlideshowImages.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import image1 from "../../assets/images/image1.jpg";
-import image4 from "../../assets/images/image4.jpg";
+import image2 from "../../assets/images/image2.jpg";
+import image3 from "../../assets/images/image3.jpg";
+import image4 from "../../assets/images/IMG_6047.jpg";
 import image5 from "../../assets/images/image5.jpg";
 import image6 from "../../assets/images/image6.jpg";
 import image7 from "../../assets/images/image7.jpg";
@@ -9,13 +11,13 @@ import "aos/dist/aos.css";
 
 const SlideshowImages = () => {
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
-  const slides = [image1, image4, image5, image6, image7];
+  const slides = [image1, image2, image3, image4, image5, image6, image7];
   const currentSlide = (n) => {
     let newSlideIndex = currentSlideIndex + n;
     if (newSlideIndex >= slides.length) {
-      newSlideIndex = 1;
+      newSlideIndex = 0;
     }
-    if (newSlideIndex < 1) {
+    if (newSlideIndex < 0) {
       newSlideIndex = slides.length - 1;
     }
     setCurrentSlideIndex(newSlideIndex);
