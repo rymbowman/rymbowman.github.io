c88c2f00a37beec11c1923dba42018cba4046717
Ryan Bowman <rymbowman@gmail.com>
2025-02-05 09:02:09 -0500

    update import syntax for image4.jpg

--- src/components/about/SlideshowImages.jsx
+++ src/components/about/SlideshowImages.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import image1 from "../../assets/images/image1.jpg";
 import image2 from "../../assets/images/image2.jpg";
 import image3 from "../../assets/images/image3.jpg";
-import image4 from "../../assets/images/IMG_6047.jpg";
+import image4 from "../../assets/images/image4.jpg";
 import image5 from "../../assets/images/image5.jpg";
 import image6 from "../../assets/images/image6.jpg";
 import image7 from "../../assets/images/image7.jpg";
